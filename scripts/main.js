0ccd92de8928b2a6cd271824422ee145f4b3f582
MajoSissi <all.hail.lvib@gmail.com>
2026-06-23 10:59:03 +0800

    update scripts

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,6 @@ const { logError } = require('./utils');
 async function main() {
     try {
         await processSources();
-        updateReadme();
     } catch (err) {
         logError(err);
         process.exit(1);
